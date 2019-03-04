03021a393c6933f9b0dcf7c1a513dd4c38b8f8ea
Ian Duke <ian@koho.ca>
2019-03-03 22:38:04 -0500

    dropdown for region select

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,52 @@
 $(document).ready(function () {
 
-    // fetches clubs from http://localhost:8080/api/all
-    const loadClubs = (cb) => {
+    let allClubsFetched = false;
+    let allClubs = null;
+
+    $("#regionSelect").on("change", function() {
+        const allClubsOption = "A";
+        let opt = $(this).find('option:selected')[0];
+
+        if (!opt.value) {
+            return;
+        }
+
+        if (allClubsFetched) {
+            // avoid unneccesary API call; use previously fetched data
+            if (opt.value == allClubsOption) {
+                return renderClubs(allClubs);
+            } else {
+                return renderClubs(allClubs.filter(club => club.region == opt.value));
+            }
+        }
+
+        if (opt.value == allClubsOption) {
+            return loadAllClubs(renderClubs)
+        } else {
+            return loadClubsByRegion(opt.value, renderClubs);
+        }
+    });
+
+    // fetches clubs from http://localhost:8080/api/club/all
+    const loadAllClubs = (cb) => {
         $.getJSON({
             url: '/api/club/all',
             success: (response) => {
+                allClubs = response;
+                allClubsFetched = true;
                 cb(response);
             },
-            fail: handleError('loadClubs')
+            fail: handleError('loadAllClubs')
+        })
+    }
+
+    const loadClubsByRegion = (region, cb) => {
+        $.getJSON({
+            url: `/api/region/${region}`,
+            success: (response) => {
+                cb(response);
+            },
+            fail: handleError('loadClubsByRegion')
         })
     }
 
@@ -16,6 +55,7 @@ $(document).ready(function () {
         const clubs = data.map((club) => {
             return generateHtmlString(club);
         })
+        $("#clubList").html('');
         $("#clubList").prepend(clubs.join(''));
     }
 
@@ -32,6 +72,7 @@ $(document).ready(function () {
           <div class="card club shadow">
             <h5 class="card-header">${name}</h5>
             <div class="card-body">
+                <!-- <img class="card-img-top" src="https://via.placeholder.com/286x180"> -->
                 <p>
                     ${address_1 ? `${address_1}<br>` : ''}
                     ${address_2}<br>
@@ -48,6 +89,7 @@ $(document).ready(function () {
                     ${twitter_handle ? `<a href="https://twitter.com/${twitter_handle}"><i class="fab fa-twitter-square"></i></a>` : ''}
                     ${instagram_handle ? `<a href="https://instagram.com/${instagram_handle}"><i class="fab fa-instagram"></i></a>` : ''}
                 </div>
+                <!-- <a href="#" class="btn btn-secondary">More Info</a> -->
             </div>
             <div class="mapbox">
                <!-- ${generateMapString(mapString)} -->
@@ -69,7 +111,4 @@ $(document).ready(function () {
                 src="https://www.google.com/maps/embed/v1/search?q=${address}" allowfullscreen>
             </iframe>`;
     }
-
-    // call method on page load
-    loadClubs(renderClubs);
 }); 
